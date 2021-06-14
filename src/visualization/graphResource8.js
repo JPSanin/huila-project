8cd4aaf1cfcc3c3861d0eb609e25180ce5e93439
Natalia Betancourt <nataliabetancourtm@gmail.com>
2021-06-13 20:36:27 -0500

    Graphs for resources

---
+++ src/visualization/graphResource8.js
@@ -0,0 +1,45 @@
+let graph8 = document.getElementById("graph8");
+let sampleGraph8 = document.getElementById("sampleGraph8");
+
+let sample8 = 0;
+
+const dataGraph8 = {
+    labels: ["Cantidad"],
+    datasets: [
+      {
+        label: 'Prestados',
+        data: [10],
+        backgroundColor:'#003F63',
+      },
+      {
+        label: 'Por prestar',
+        data: [23],
+        backgroundColor: '#008892',
+      },
+    ]
+  };
+  
+  const configGraph8 = {
+      type: 'bar',
+      data: dataGraph8,
+      options: {
+        plugins: {
+          title: {
+            display: true,
+            text: 'Total de perifericos por sede '
+          },
+        },
+        responsive: true,
+        scales: {
+          x: {
+            stacked: true,
+          },
+          y: {
+            stacked: true
+          }
+        }
+      }
+    };
+
+    let graph8Visible = new Chart(graph8, configGraph8);
+    sampleGraph8.innerHTML="Muestra "+ sample8; 
